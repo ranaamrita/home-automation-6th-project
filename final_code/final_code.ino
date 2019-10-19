84253d8279f37dc9570fcfc2d9abc1f4cda97457
aishu112 <aishwarya3rawat@gmail.com>
2019-10-19 12:26:57 +0545

    Finalize the all code .

--- final_code/final_code.ino
+++ final_code/final_code.ino
@@ -1,11 +1,18 @@
-// code for ultrasonic
+// pin are defined for mq2
 
-#define trigPin 7
-#define echoPin 8
-#define led 13
-#define fan 4
+int led_pin = 5;
+int sensor_pin = A0;
+int siron_mq = 6;
 
-// Pin are defined for ultrasonic in above
+
+// Pin are defined for ultrasonic
+
+int siron_ult = 7;
+#define trigPin 8
+#define echoPin 9
+#define led 10
+
+// code 
 
 void setup(){
   
@@ -15,10 +22,20 @@ void setup(){
   pinMode (trigPin, OUTPUT);
   pinMode (echoPin, INPUT);
   pinMode (led, OUTPUT);
-  pinMode (fan, OUTPUT);
+  pinMode(siron_ult, OUTPUT);
+
+ // code for mq2
+
+ Serial.begin(9600);
+ pinMode(led_pin, OUTPUT);
+ pinMode(siron_mq, OUTPUT);
+ pinMode(sensor_pin, INPUT);
 }
 
 void loop(){
+
+  // code for ultrasonic
+  
   long duration, distance;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -27,12 +44,28 @@ void loop(){
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
-  if (distance < 50){
-     digitalWrite(led,HIGH);
-      digitalWrite(4,HIGH); 
+  if (distance > 50){
+      digitalWrite(led,HIGH);
+      digitalWrite(siron_ult,HIGH); 
       Serial.print(distance);
       Serial.println("cm");
       Serial.println("Out of Range");
   }
   delay(500);
+
+  //code of ultrasonic finished.
+
+   // code of mq2 starts
+
+  int sensor_pin = analogRead(sensor_pin);
+  Serial.println(sensor_pin);
+  if(sensor_pin >= 32){
+    digitalWrite(led_pin, HIGH);
+    digitalWrite(siron_mq, HIGH);
+  }
+  else{
+    digitalWrite(led_pin, LOW);
+    digitalWrite(siron_mq, LOW);
+  }
+  delay(1000);
 }
